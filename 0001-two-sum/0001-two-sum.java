b66004f7ae7fc7343848fa6274370ba5a3cd693c
superco01 <32862937+superco01@users.noreply.github.com>
2024-03-19 00:48:37 +0800

    Time: 2 ms (97.91%), Space: 44.7 MB (74.42%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // return iterative(nums, target);
         return hashMap(nums, target);
     }
 
@@ -16,23 +15,6 @@ class Solution {
                 return result;
             }
             map.put(nums[i], i);
-        }
-        return result;
-    }
-
-    private int[] iterative(int[] nums, int target) {
-        int[] result = new int[2];
-        for(int i = 0; i < nums.length; i++) {
-            result[0] = i;
-            for(int j = 0; j < nums.length; j++) {
-                if(i == j) {
-                    continue;
-                }
-                if(nums[i] + nums[j] == target) {
-                    result[1] = j;
-                    return result;
-                }
-            }
         }
         return result;
     }
