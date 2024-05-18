babe353abe42a1cc19dbfaacea9cfeedea7a9891
superco01 <32862937+superco01@users.noreply.github.com>
2024-05-18 10:21:41 +0700

    Time: 31 ms (42.33%), Space: 45.6 MB (37.6%) - LeetHub

--- 2384-largest-palindromic-number/2384-largest-palindromic-number.java
+++ 2384-largest-palindromic-number/2384-largest-palindromic-number.java
@@ -1,29 +1,27 @@
 class Solution {
     public String largestPalindromic(String num) {
-         Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (char c : num.toCharArray()) {
-            map.merge((int) c - (int) '0', 1, Integer::sum);
+            map.merge(c - '0', 1, Integer::sum);
         }
-        final int[] number = {0};
-        final String[] strings = {"", ""};
-        StringBuilder end = new StringBuilder();
+        AtomicReference<Integer> middleNum = new AtomicReference<>(0);
+        StringBuilder right = new StringBuilder();
         map.forEach((key, value) -> {
-            if (value % 2 == 1)
-                number[0] = Math.max(number[0], key);
+            if (value % 2 == 1) {
+                middleNum.set(Math.max(middleNum.get(), key));
+            }
             for (int i = 0; i < value / 2; i++) {
-                end.append(key);
+                right.append(key);
             }
         });
-        if (end.lastIndexOf("0") == end.length() - 1)
-            return String.valueOf(number[0]);
-        StringBuilder start = new StringBuilder(end);
-        start.reverse();
-        if (end.length() * 2 == num.length()) {
-            start.append(end);
-            return start.toString();
+        String finalMiddleNum = String.valueOf(middleNum.get());
+        var left = new StringBuilder(right).reverse();
+        if (right.isEmpty() || right.charAt(right.length() - 1) == '0') {
+            return finalMiddleNum;
         }
-        start.append(number[0]);
-        start.append(end);
-        return start.toString();
+        if (right.length() * 2 == num.length()) {
+            return left.append(right).toString();
+        }
+        return left.append(finalMiddleNum).append(right).toString();
     }
 }
