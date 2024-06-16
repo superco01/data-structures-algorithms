3745fe16f684b41031416939107d0dddf861460d
superco01 <32862937+superco01@users.noreply.github.com>
2024-06-16 17:24:36 +0800

    Time: 70 ms (6.11%), Space: 45.7 MB (7.96%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minDeletions(String s) {
+        Map<Character, Integer> charFrequencyMap = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            charFrequencyMap.merge(c, 1, Integer::sum);
+        }
+
+        System.out.println(charFrequencyMap);
+        HashSet<Integer> set = new HashSet<>();
+        int deletions = 0;
+        for (int frequency : charFrequencyMap.values()) {
+            while (frequency > 0 && set.contains(frequency)) {
+                frequency--;
+                deletions++;
+            }
+            set.add(frequency);
+        }
+
+        return deletions;
+    }
+}
