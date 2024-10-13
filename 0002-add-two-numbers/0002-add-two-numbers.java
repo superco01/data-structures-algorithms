a7de6e497c7d33354669baed551fafd75963f8b9
superco01 <32862937+superco01@users.noreply.github.com>
2024-10-13 22:47:04 +0700

    Time: 1 ms (100%), Space: 44.4 MB (41.53%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,32 +1,32 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        var currentNode = new ListNode();
-        var result = currentNode;
-        var carry = 0;
-        while(l1 != null || l2 != null || carry != 0) {
-            var value = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0);
-            value += carry;
-            carry = value / 10;
-
-            var newNode = new ListNode(value % 10);
-            currentNode.next = newNode;
-            currentNode = newNode;
-
-            l1 = (l1 != null ? l1.next : null);
-            l2 = (l2 != null ? l2.next : null);
+        var result = new ListNode();
+        var temp = new ListNode(0, result);
+        var excess = 0;
+        while (l1 != null || l2 != null || excess != 0) {
+            var sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + excess;
+            excess = 0;
+            if (sum >= 10) {
+                excess = sum / 10;
+                result.next = new ListNode(sum % 10);
+            } else {
+                result.next = new ListNode(sum);
+            }
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
+            result = result.next;
         }
 
-        result = result.next;
-        return result;
+        return temp.next.next;
     }
 }
