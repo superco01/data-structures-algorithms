3d33d581a35069c6fb465eab3adf8c282288c4a6
superco01 <32862937+superco01@users.noreply.github.com>
2024-10-13 22:03:05 +0700

    Time: 44 ms (39.86%), Space: 45.1 MB (10.42%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,20 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        return hashMap(nums, target);
-    }
-
-    private int[] hashMap(int[] nums, int target) {
-        int[] result = new int[2];
-        Map<Integer, Integer> map = new HashMap<>();
-        int reqNum = 0;
-        for(int i = 0; i < nums.length; i++) {
-            reqNum = target - nums[i];
-            if(map.get(reqNum) != null) {
-                result[0] = i;
-                result[1] = map.get(reqNum);
-                return result;
+        var result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
             }
-            map.put(nums[i], i);
         }
         return result;
     }
