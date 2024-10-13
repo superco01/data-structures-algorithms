64a8b40c5ce0b53e5c4baa178f521804d6973514
superco01 <32862937+superco01@users.noreply.github.com>
2024-10-13 14:24:15 +0700

    Time: 1 ms (100%), Space: 42.1 MB (87.23%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -1,34 +1,35 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        // maintain an unchanging reference to node ahead of the return node.
-        ListNode prehead = new ListNode(-1);
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        var head = new ListNode();
+        var temp = new ListNode(0, head);
 
-        ListNode prev = prehead;
-        while (l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
-                prev.next = l1;
-                l1 = l1.next;
+        while (!(null == list1 && null == list2)) {
+            if (null == list1) {
+                head.next = new ListNode(list2.val);
+                list2 = list2.next;
+            } else if (null == list2) {
+                head.next = new ListNode(list1.val);
+                list1 = list1.next;
+            } else if (list1.val <= list2.val) {
+                head.next = new ListNode(list1.val);
+                list1 = list1.next;
             } else {
-                prev.next = l2;
-                l2 = l2.next;
+                head.next = new ListNode(list2.val);
+                list2 = list2.next;
             }
-            prev = prev.next;
+            head = head.next;
         }
 
-        // At least one of l1 and l2 can still have nodes at this point, so connect
-        // the non-null list to the end of the merged list.
-        prev.next = l1 == null ? l2 : l1;
-
-        return prehead.next;
+        return temp.next.next;
     }
 }
