1b6bcbcf4c4f5e2bc8ecd4c1465b87e33285457c
superco01 <32862937+superco01@users.noreply.github.com>
2024-11-03 20:57:15 +0700

    Time: 7 ms (14.29%), Space: 42.2 MB (7.8%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -6,13 +6,8 @@ class Solution {
         for (int i = 0; i < strs.length; i++) {
             minLength = Math.min(minLength, strs[i].length());
         }
-        System.out.println("ML:" + minLength);
-        // if (strs[0].equals("") || strs.length == 1) {
-        //     return result;
-        // }
 
         while (currentCharIndex < minLength) {
-            System.out.println("CI:" + currentCharIndex);
             for (int i = 0; i < strs.length; i++) {
                 if (!(strs[0].charAt(currentCharIndex) == strs[i].charAt(currentCharIndex))) {
 
