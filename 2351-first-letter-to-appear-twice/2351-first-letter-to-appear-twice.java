c0e796950e9e6bb397ec9e442c00e93267f9d644
superco01 <32862937+superco01@users.noreply.github.com>
2024-11-25 11:27:13 +0700

    Time: 0 ms (100%), Space: 41.3 MB (68.66%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -1,9 +1,10 @@
 class Solution {
     public char repeatedCharacter(String s) {
-        Map map = new HashMap<Character, Integer>();
+        Map map = new HashMap<Character, Boolean>();
         for (char ch : s.toCharArray()) {
-            if (map.get(ch) == null) {
-                map.put(ch, 0);
+            var exist = map.get(ch);
+            if (exist == null) {
+                map.put(ch, true);
             } else {
                 return ch;
             }
