650c53de4227b0d870382fd8eed5975f4a0108e2
superco01 <32862937+superco01@users.noreply.github.com>
2026-07-26 19:09:33 +0900

    Time: 647 ms (5.16%), Space: 110 MB (6.12%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -2,15 +2,12 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         var countMap = new HashMap<Integer, Integer>();
         for (int i = 0; i < nums.length; i++) {
-            countMap.put(nums[i], countMap.getOrDefault(nums[i], 0) + 1);
-            // System.out.println(countMap.get(nums[i]));
-        }
-
-        for (int count : countMap.values()) {
+            var count = countMap.get(nums[i]);
             System.out.println(count);
-            if (count > 1) {
+            if (count != null) {
                 return true;
             }
+            countMap.put(nums[i], 1);
         }
 
         return false;
